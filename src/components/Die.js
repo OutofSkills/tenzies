222b0ffcb6cd47817b7a54c940b464d1c072d1f6
OutofSkills <56539829+OutofSkills@users.noreply.github.com>
2022-06-29 15:39:17 +0300

    changed values of the dice to dots

--- src/components/Die.js
+++ src/components/Die.js
@@ -3,13 +3,29 @@ export default function Die(props) {
     const styles = {
         backgroundColor: props.isHeld ? "#59E391" : "white"
     }
+
+    const dotPositions = {
+        1: ['dot__middle dot__center'],
+        2: ['dot__top dot__left', 'dot__bottom dot__right'],
+        3: ['dot__top dot__left', 'dot__middle dot__center', 'dot__bottom dot__right'],
+        4: ['dot__top dot__left', 'dot__top dot__right', 'dot__bottom dot__left', 'dot__bottom dot__right'],
+        5: ['dot__top dot__left', 'dot__top dot__right', 'dot__middle dot__center', 'dot__bottom dot__left', 'dot__bottom dot__right'],
+        6: ['dot__top dot__left', 'dot__top dot__right', 'dot__middle dot__right', 'dot__bottom dot__left', 'dot__bottom dot__right',  'dot__middle dot__left']
+    }
+
+    const dots = dotPositions[props.value];
+    console.log(dots)
+    const dieDotsElements = dots.map(dot => {
+        return <div className={`dot ${dot}`}></div>
+    })
+
     return (
         <div 
             className="die-face" 
             style={styles}
             onClick={props.holdDice}
         >
-            <h2 className="die-num">{props.value}</h2>
+            {dieDotsElements}
         </div>
     )
 }
